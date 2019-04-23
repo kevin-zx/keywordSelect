2013a423a6015896875efb08d5150ca16a715e3a
kevin-zx <Zx@^240228>
2019-04-23 16:02:05 +0800

    按包实现

--- category_words/expand_category_word_test.go
+++ category_words/expand_category_word_test.go
@@ -3,7 +3,7 @@ package category_words
 import "testing"
 
 func TestGetCategoryWords(t *testing.T) {
-	err := GetCategoryWords("www.gametea.com", []string{"麻将游戏", "游戏大厅", "棋牌游戏", "斗牛", "双扣", "拼十", "斗地主"})
+	err := GetCategoryWords("www.gametea.com", []string{"麻将游戏", "游戏大厅", "棋牌游戏", "斗牛", "双扣", "拼十", "斗地主"}, "")
 	if err != nil {
 		panic(err)
 	}
