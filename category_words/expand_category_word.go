67ea6db93d427051d57dedc6fbb534370cd15025
kevin-zx <Zx@^240228>
2019-04-28 15:04:43 +0800

    时间计算有点错误

--- category_words/expand_category_word.go
+++ category_words/expand_category_word.go
@@ -214,8 +214,8 @@ func createFile(path string, fileName string) (recentQuery bool, rFile *os.File,
 }
 
 func timeSub(t1, t2 time.Time) int {
-	t1 = time.Date(t1.Year(), t1.Month(), t1.Day(), 0, 0, 0, 0, time.Local)
-	t2 = time.Date(t2.Year(), t2.Month(), t2.Day(), 0, 0, 0, 0, time.Local)
+	t1 = time.Date(t1.Year(), t1.Month(), t1.Day(), t1.Hour(), 0, 0, 0, time.Local)
+	t2 = time.Date(t2.Year(), t2.Month(), t2.Day(), t2.Hour(), 0, 0, 0, time.Local)
 
 	return int(t1.Sub(t2).Hours())
 }
